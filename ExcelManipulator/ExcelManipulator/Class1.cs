9212f7d09e280d107c38ce09c315b20d0df7dada
acutec-ldawson <ldawson@acutecprecision.com>
2017-06-21 09:44:14 -0400

    Initialization: Added in files

---
+++ ExcelManipulator/ExcelManipulator/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+
+namespace ExcelManipulator
+{
+    public class ExcelReader
+    {
+    }
+}
---
+++ ExcelManipulator/ExcelManipulator/ExcelManipulator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelManipulator
+{
+    public class ExcelReader
+    {
+
+        string filePath = "";
+        public ExcelReader() {
+        }
+        public ExcelReader(string _filePath) {
+            filePath = _filePath;
+        }
+        public void SetFile(string _filePath)
+        {
+            filePath = _filePath;
+        }
+        public void toCSV() {
+            Type officeType = Type.GetTypeFromProgID("Excel.Application");
+            if (officeType == null)
+            {
+                //Can't access Excel
+            }
+            else {
+                Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+
+                app.DisplayAlerts = false;
+
+                Microsoft.Office.Interop.Excel.Workbook workbook = app.Workbooks.Open(filePath);
+                foreach (Worksheet sheet in workbook.Sheets)
+                {
+                    string csvFile = Path.ChangeExtension(filePath, "_"+sheet.Name+".csv");
+                    sheet.SaveAs(csvFile, Microsoft.Office.Interop.Excel.XlFileFormat.xlCSV);
+                }
+
+                workbook.Close();
+                app.Quit();
+            }
+        }
+
+
+    }
+}
